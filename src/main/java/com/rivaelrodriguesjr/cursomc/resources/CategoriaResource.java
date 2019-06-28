2056fb91dbcefecc20830f5eb34ce77a8d4c436d
Rivael Rodrigues Jr <rivael.rodriguesjr@outlook.com>
2019-06-28 15:41:27 -0300

    Testando a primeira classe de domínio

--- src/main/java/com/rivaelrodriguesjr/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/rivaelrodriguesjr/cursomc/resources/CategoriaResource.java
@@ -1,9 +1,14 @@
 package com.rivaelrodriguesjr.cursomc.resources;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.rivaelrodriguesjr.cursomc.domain.Categoria;
 
 
 @RestController
@@ -11,9 +16,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class CategoriaResource {
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public ResponseEntity<String> listar(){
+	public ResponseEntity<List<Categoria>> listar(){
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritório");
 		
-		return ResponseEntity.ok().body("Rest está funcionado!");
+		return ResponseEntity.ok().body(Arrays.asList(cat1, cat2));
 	}
 
 }
