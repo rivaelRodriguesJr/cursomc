70a126346303fdd5dafc5efdc97fa8af6ef66567
rivael.rodriguesjr <rrjunior@fitec.org.br>
2019-07-12 11:19:14 -0300

    Pedido, EstadoPagamento e Pagamento - parte 2

--- src/main/java/com/rivaelrodriguesjr/cursomc/domains/PagamentoComCartao.java
+++ src/main/java/com/rivaelrodriguesjr/cursomc/domains/PagamentoComCartao.java
@@ -1,7 +1,10 @@
 package com.rivaelrodriguesjr.cursomc.domains;
 
+import javax.persistence.Entity;
+
 import com.rivaelrodriguesjr.cursomc.domains.enums.EstadoPagamento;
 
+@Entity
 public class PagamentoComCartao extends Pagamento {
 	private static final long serialVersionUID = 1L;
 
